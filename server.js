50c27f629224fd959c1176c905dcac648fd9f274
Dave Seidman <daveseidman@gmail.com>
2016-03-23 14:17:49 +0000

    adding gpio

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ var express = require('express');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var colors = require('colors');
+var gpio = require("gpio");
+
+var gpio2 = gpio.export(2, { direction:'out', ready: function() { gpio2.set(0); } });
+var gpio3 = gpio.export(3, { direction:'out', ready: function() { gpio3.set(0); } });
+var gpio4 = gpio.export(4, { direction:'out', ready: function() { gpio4.set(0); } });
+var gpio14 = gpio.export(14, { direction:'out', ready: function() { gpio14.set(0); } });
+
+var gpioArray = [gpio2,gpio3,gpio4,gpio14];
 
 var rooms = []; // maintain a list of rooms
-var dir = ['lf','up','rt','dn'];
+var dir = ['◀','▲','▶','▼'];
 var offset = [
     { x:-1, y:0 },
     { x:0, y:-1 },
@@ -37,10 +45,14 @@ io.on('connection', function (socket) {
 
         position.x += offset[data.key].x;
         position.y += offset[data.key].y;
-
         data.position = position;
+
+        gpioArray[data.key].set(1);
+
         socket.emit('keydown', data);
-        console.log(dir[data.key], "pressed");
+        console.log(dir[data.key], "◼");
+
+
     });
 
     socket.on('keyup', function(data) {
@@ -48,8 +60,11 @@ io.on('connection', function (socket) {
         // send the signal right back to client to confirm it's been received
         // when the claw machine is attached to pi, this event should also
         // return the updated position of the claw.
+
+        gpioArray[data.key].set(0);
+
         socket.emit('keyup', data);
-        console.log(dir[data.key], "released");
+        console.log(dir[data.key], "◻");
     });
 
 });
